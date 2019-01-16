59f0aee2cd9bba22909815c8a8b6078e5f628f90
krdowns <krjdowns16@gmail.com>
2019-01-16 14:19:22 -0800

    cors

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const passport = require('./config/passport')()
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-
 //controllers
 const userController = require('./controllers/users.js')
 const postsController = require('./controllers/posts.js')
 const citiesController = require('./controllers/cities.js')
 const app = express()
 
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
 //middleware
-app.use(cors())
+// app.use(cors())
+app.use(allowCrossDomain)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(bodyParser.json())
